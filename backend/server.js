b262e3a1e6b4ec2ca5389b40a85ffe97b837504e
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-10 20:18:09 +0200

    add database connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express")
+const db = require("./config/db")
 
 const app = express()
 
-app.use = (express.json())
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hire flow backend attivo!")
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log("Connessione al databaseb fallita!")
+        return
+    }
+    console.log("Connessione al database riuscita!");
 })
 
 app.listen(3000, () => {
