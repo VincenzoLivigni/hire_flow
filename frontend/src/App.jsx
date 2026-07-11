f790f0150d18acdbb9985d576742188f74a5699a
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-11 19:12:20 +0200

    setup React routing and base components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { GlobalProvider } from "./contexts/GlobalContext"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+
 function App() {
 
   return (
     <>
-
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   )
 }
