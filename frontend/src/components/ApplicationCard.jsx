bbae10f509117235a317c31d39c4edf123037426
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-12 21:50:48 +0200

    add modal component for editing application

--- frontend/src/components/ApplicationCard.jsx
+++ frontend/src/components/ApplicationCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import { deleteApplication, updateApplication } from "../services/api";
 import { useState } from "react";
 import DeleteApplicationModal from "./DeleteApplicationModal";
+import EditApplicationModal from "./EditApplicationModal";
 
 export default function ApplicationCard({ application, onDelete, onUpdate }) {
 
@@ -14,35 +14,6 @@ export default function ApplicationCard({ application, onDelete, onUpdate }) {
 
     // MODIFICO UNA APPLICATION
     const [editApplication, setEditApplication] = useState(false)
-
-    const [company, setCompany] = useState(application.company_name)
-    const [role, setRole] = useState(application.role)
-    const [location, setLocation] = useState(application.location)
-    const [notes, setNotes] = useState(application.notes)
-    const [link_job, setLink_job] = useState(application.link_job)
-    const [status, setStatus] = useState(application.status)
-
-    async function handleUpdate(e) {
-        e.preventDefault()
-
-        try {
-            await updateApplication(
-                application.id,
-                company,
-                role,
-                location,
-                notes,
-                link_job,
-                status
-            )
-
-            setEditApplication(false)
-            onUpdate()
-        }
-        catch (err) {
-            console.log("Error: ", err)
-        }
-    }
 
     // ELIMINO UNA APPLICATION
     const [confirmDelete, setConfirmDelete] = useState(false)
@@ -68,71 +39,11 @@ export default function ApplicationCard({ application, onDelete, onUpdate }) {
 
             {
                 editApplication && (
-                    <form onSubmit={handleUpdate}>
-                        <div>
-                            <label>Azienda</label>
-                            <input
-                                type="text"
-                                placeholder="Modifica azienda"
-                                value={company}
-                                onChange={(e) => setCompany(e.target.value)}
-                            />
-                        </div>
-
-                        <div>
-                            <label>Posizione</label>
-                            <input
-                                type="text"
-                                placeholder="Modifica posizione"
-                                value={role}
-                                onChange={(e) => setRole(e.target.value)}
-                            />
-                        </div>
-
-                        <div>
-                            <label>Luogo</label>
-                            <input
-                                type="text"
-                                placeholder="Modifica luogo"
-                                value={location}
-                                onChange={(e) => setLocation(e.target.value)}
-                            />
-                        </div>
-
-                        <div>
-                            <label>Link candidatura</label>
-                            <input
-                                type="text"
-                                placeholder="Modifica link candidatura"
-                                value={link_job}
-                                onChange={(e) => setLink_job(e.target.value)}
-                            />
-                        </div>
-
-                        <div>
-                            <label>Stato</label>
-                            <select
-                                value={status}
-                                onChange={(e) => setStatus(e.target.value)}
-                            >
-                                <option value="applied">Candidatura inviata</option>
-                                <option value="interview">Colloquio</option>
-                                <option value="offer">Offerta ricevuta</option>
-                                <option value="rejected">Rifiutata</option>
-                            </select>
-                        </div>
-
-                        <div>
-                            <label>Note</label>
-                            <textarea
-                                placeholder="Modifica note"
-                                value={notes}
-                                onChange={(e) => setNotes(e.target.value)}
-                            />
-                        </div>
-
-                        <button type="submit">Modifica candidatura</button>
-                    </form>
+                    <EditApplicationModal
+                        application={application}
+                        onClose={() => setEditApplication(false)}
+                        onUpdate={onUpdate}
+                    />
                 )
             }
 
