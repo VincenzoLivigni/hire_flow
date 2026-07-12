0469a0c12903437f9c0269ac8af2e37f1a5eea5b
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-12 23:37:11 +0200

    add application filters

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -36,6 +36,26 @@ export default function Dashboard() {
 
     }, [token])
 
+    // FILTRI
+    const [searchCompany, setSearchCompany] = useState("")
+    const [searchRole, setSearchRole] = useState("")
+    const [filterLocation, setFilterLocation] = useState("Tutti")
+    const [filterStatus, setFilterStatus] = useState("Tutti")
+
+    const filteredApplications = applications.filter((a) => {
+        return (
+            a.company_name.toLowerCase().includes(searchCompany.toLocaleLowerCase()) &&
+            a.role.toLowerCase().includes(searchRole.toLowerCase()) &&
+            (filterLocation === "Tutti" || a.location === filterLocation) &&
+            (filterStatus === "Tutti" || a.status === filterStatus)
+        )
+    })
+
+    // Luogo
+    const locations = [
+        ...new Set(applications.map((a) => a.location))
+    ]
+
     return (
         <>
             <Header />
@@ -44,9 +64,63 @@ export default function Dashboard() {
 
             <ApplicationForm onCreated={fetchApplications} />
 
+            <div className="filters">
+
+                <div>
+                    <label>Cerca Azienda</label>
+                    <input
+                        type="text"
+                        placeholder="Cerca..."
+                        value={searchCompany}
+                        onChange={(e) => setSearchCompany(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Cerca posizione</label>
+                    <input
+                        type="text"
+                        placeholder="Cerca..."
+                        value={searchRole}
+                        onChange={(e) => setSearchRole(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Filtra per luogo</label>
+                    <select
+                        value={filterLocation}
+                        onChange={(e) => setFilterLocation(e.target.value)}
+                    >
+                        <option value="Tutti">Tutti</option>
+                        {
+                            locations.map((city) => (
+                                <option key={city}>
+                                    {city}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </div>
+
+                <div>
+                    <label>Filtra per stato</label>
+                    <select
+                        value={filterStatus}
+                        onChange={(e) => setFilterStatus(e.target.value)}
+                    >
+                        <option value="Tutti">Tutti</option>
+                        <option value="applied">Candidatura inviata</option>
+                        <option value="interview">Colloquio</option>
+                        <option value="offer">Offerta ricevuta</option>
+                        <option value="rejected">Rifiutata</option>
+                    </select>
+                </div>
+            </div>
+
             <div>
                 {
-                    applications.map((a) => (
+                    filteredApplications.map((a) => (
                         <div key={a.id}>
                             <ApplicationCard
                                 application={a}
