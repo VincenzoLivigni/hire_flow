f6f5e893d3ded0add0c2048bf376b65d1ba29872
VincenzoLivigni <enzolivigni13@gmail.com>
2026-07-12 21:38:51 +0200

    implement update application flow

--- frontend/src/components/ApplicationCard.jsx
+++ frontend/src/components/ApplicationCard.jsx
@@ -1,15 +1,47 @@
 import { Link } from "react-router-dom";
-import { deleteApplication } from "../services/api";
+import { deleteApplication, updateApplication } from "../services/api";
 import { useState } from "react";
 import DeleteApplicationModal from "./DeleteApplicationModal";
 
-export default function ApplicationCard({ application, onDelete }) {
+export default function ApplicationCard({ application, onDelete, onUpdate }) {
 
     const statusLabels = {
         applied: "Candidatura inviata",
         interview: "Colloquio",
         offer: "Offerta ricevuta",
         rejected: "Rifiutata"
+    }
+
+    // MODIFICO UNA APPLICATION
+    const [editApplication, setEditApplication] = useState(false)
+
+    const [company, setCompany] = useState(application.company_name)
+    const [role, setRole] = useState(application.role)
+    const [location, setLocation] = useState(application.location)
+    const [notes, setNotes] = useState(application.notes)
+    const [link_job, setLink_job] = useState(application.link_job)
+    const [status, setStatus] = useState(application.status)
+
+    async function handleUpdate(e) {
+        e.preventDefault()
+
+        try {
+            await updateApplication(
+                application.id,
+                company,
+                role,
+                location,
+                notes,
+                link_job,
+                status
+            )
+
+            setEditApplication(false)
+            onUpdate()
+        }
+        catch (err) {
+            console.log("Error: ", err)
+        }
     }
 
     // ELIMINO UNA APPLICATION
@@ -28,8 +60,81 @@ export default function ApplicationCard({ application, onDelete }) {
                 </p>
                 <p><strong>Note:</strong> {application.notes}</p>
 
-                <button onClick={() => setConfirmDelete(true)}>Elimina</button>
+                <div>
+                    <button onClick={() => setEditApplication(true)}>Modifica</button>
+                    <button onClick={() => setConfirmDelete(true)}>Elimina</button>
+                </div>
             </div>
+
+            {
+                editApplication && (
+                    <form onSubmit={handleUpdate}>
+                        <div>
+                            <label>Azienda</label>
+                            <input
+                                type="text"
+                                placeholder="Modifica azienda"
+                                value={company}
+                                onChange={(e) => setCompany(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label>Posizione</label>
+                            <input
+                                type="text"
+                                placeholder="Modifica posizione"
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label>Luogo</label>
+                            <input
+                                type="text"
+                                placeholder="Modifica luogo"
+                                value={location}
+                                onChange={(e) => setLocation(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label>Link candidatura</label>
+                            <input
+                                type="text"
+                                placeholder="Modifica link candidatura"
+                                value={link_job}
+                                onChange={(e) => setLink_job(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label>Stato</label>
+                            <select
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}
+                            >
+                                <option value="applied">Candidatura inviata</option>
+                                <option value="interview">Colloquio</option>
+                                <option value="offer">Offerta ricevuta</option>
+                                <option value="rejected">Rifiutata</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <label>Note</label>
+                            <textarea
+                                placeholder="Modifica note"
+                                value={notes}
+                                onChange={(e) => setNotes(e.target.value)}
+                            />
+                        </div>
+
+                        <button type="submit">Modifica candidatura</button>
+                    </form>
+                )
+            }
 
             {
                 confirmDelete && (
